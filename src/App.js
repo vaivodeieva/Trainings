b884c733c61ae33259931f6666252d0faf7800c2
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-07-09 17:17:17 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,41 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 function App() {
   return (
     <div>
+      <Router>
+            <Header />
+                <Switch>
+
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+
+                    <Route path="/articles">
+                        <Article />
+                    </Route>
+
+                    <Route path="/article">
+                        <Tags />
+                    </Route>
+
+                    <Route path="/chat">
+                        <Tags />
+                    </Route>
+
+                    <Route path="/register">
+                        <Tags />
+                    </Route>
+
+                    <Route path="/aboutus">
+                        <Tags />
+                    </Route>
+
+                    <Route>
+                        <Page404 />
+                    </Route>
+
+                </Switch>
+                <Footer />
+            </Router>
      
     </div>
   );
