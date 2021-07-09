a9990672e776d0ed1e2d67f1ddbaba50fe3d789d
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-07-09 17:36:29 +0300

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,13 +1,14 @@
 function Footer() {
 
+    let date = new Date();
+
     return (
 
         <div className="container">
             <footer className="pt-4 my-md-5 pt-md-5 border-top">
                 <div className="row">
                     <div className="col-12 col-md">
-                        <small className="d-block mb-3 text-muted text-center">© 2021</small></div>
-                
+                        <small className="d-block mb-3 text-muted text-center"> {date.getFullYear()} © All rights reserved</small></div>            
                 </div>
             </footer>
         </div>
