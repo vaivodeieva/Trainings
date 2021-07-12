0f7bcc9b4236643aa242e500165b71bec1971bf9
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-07-12 15:24:45 +0300

    update Register.js

--- src/index.js
+++ src/index.js
@@ -5,27 +5,12 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 
 
-import registerServiceWorker from './registerServiceWorker';
-
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root'));
 
-
-  registerServiceWorker();
-
-
-  // import React from 'react';
-  // import ReactDOM from 'react-dom';
-  // import './index.css';
-  // import App from './App';
-  // import registerServiceWorker from './registerServiceWorker';
-  
-  // ReactDOM.render(<App />, document.getElementById('root'));
-  // registerServiceWorker();
 
 
 
